323b7ad861806b12ba180d94037d6b8ff406a21c
Y.K <powdersugar828828@gmail.com>
2021-07-12 00:02:36 +0900

    feat: composition-apiにしなかったら一応動いた。

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -1,26 +1,32 @@
 <template>
-    <Multiselect
-      v-model="selected"
-      :options="options"
-    >
-    </Multiselect>
+  <label class="typo__label">Select with search</label>
+  <multiselect v-model="value" :options="options" :custom-label="nameWithLang" placeholder="Select one" label="name" track-by="name"></multiselect>
+  <pre class="language-json"><code>{{ value  }}</code></pre>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
 import Multiselect from 'vue-multiselect/src/Multiselect.vue';
 
-export default defineComponent({
+export default {
   components: {
-    Multiselect,
+    Multiselect
   },
-  setup() {
-    const selected = null;
-    const options = ['list', 'of', 'options'];
+  data () {
     return {
-      selected,
-      options,
+      value: { name: 'Vue.js', language: 'JavaScript' },
+      options: [
+        { name: 'Vue.js', language: 'JavaScript' },
+        { name: 'Rails', language: 'Ruby' },
+        { name: 'Sinatra', language: 'Ruby' },
+        { name: 'Laravel', language: 'PHP' },
+        { name: 'Phoenix', language: 'Elixir' }
+      ]
+    }
+  },
+  methods: {
+    nameWithLang ({ name }) {
+      return `${name}`
     }
   }
-});
+}
 </script>
